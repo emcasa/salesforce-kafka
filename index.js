bfd7ac766c4b4168cc7240e650f7766a3e6e86c1
pmargreff <pmargreff@gmail.com>
2019-07-29 18:45:28 -0300

    feature(kafka): auto-create topics on kafka if them doesn't exists
    Also using dinamic topics mapper.

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const kafka = require('./lib/kafka')
 
 const salesforce = require('./lib/salesforce')
 
+const TOPICS_MAP =
+[
+  {
+    from: '/topic/TestOpportunityUpdates',
+    to: 'salesforceA'
+  },
+  {
+    from: '/topic/AccountUpdated',
+    to: 'salesforceB'
+  }
+]
+
 async function init() {
     const producer = kafka.newKafka().producer()
     await producer.connect()
@@ -12,13 +24,29 @@ async function init() {
     const sf = await salesforce.login()
     console.log('connected to salesforce')
 
-    const topicsToSubscribe = ['/topic/TestOpportunityUpdates', '/topic/AccountUpdated']
-
-    topicsToSubscribe.forEach((topic) => {
-        salesforce.subscribe(sf, topic, (message) => {
-          kafka.produce(producer, JSON.stringify(message))
+    TOPICS_MAP.forEach((topic) => {
+        salesforce.subscribe(sf, topic.from, (message) => {
+          kafka.produce(producer, JSON.stringify(message, topic.to))
         })
     });
 }
 
+async function ensureKafkaTopicsExists(kafka, topicsMap) {
+    console.log("Checking if Kafka topics exists ...")
+    const admin = kafka.newKafka().admin()
+    await admin.connect()
+
+    const result = await kafka.createTopic(admin, TOPICS_MAP.map(({to}) => to))
+      .catch((e) => {console.log('error:', e)})
+
+    if (result === true) {
+      console.log('created new topics', topicsMap)
+    } else {
+      console.log('topics already exists');
+    }
+
+    await admin.disconnect()
+}
+
+ensureKafkaTopicsExists(kafka, TOPICS_MAP)
 init()
